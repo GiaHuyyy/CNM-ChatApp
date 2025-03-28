137f9e6015a49f58d383237ff9a522e3b3993853
Nguyen Tien Phat <imissu3403@gmail.com>
2025-03-28 19:38:15 +0700

    up

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -2,7 +2,6 @@ import { router } from "expo-router";
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "../components/CustomButton";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 export default function App() {
@@ -12,7 +11,6 @@ export default function App() {
         <View className="flex-1 bg-[#8b2369] pb-[53px]">
           <View className="px-[37px]">
             <View className="h-[58px] w-[58px] items-center justify-center rounded-full bg-white">
-              <FontAwesome5 name="angle-left" size={24} color="#8b2369" />
               <FontAwesomeIcon icon={faMessage} />
             </View>
             <Text className="mt-6 w-[280px] font-osemibold600 text-[32px] leading-[48px] text-white">
