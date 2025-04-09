6468a93a7b5c87869c4302116d99e7df1765a900
Gia Huy <tovugiahuy@gmail.com>
2025-04-09 07:24:52 +0700

    Thử nghiệm sms Twilio

--- server/routers/index.js
+++ server/routers/index.js
@@ -8,6 +8,8 @@ const updateUserDetails = require("../controller/updateUserDetails");
 const logout = require("../controller/logout");
 const searchUser = require("../controller/searchUser");
 const searchFriendUser = require("../controller/searchFriendUser");
+const { sendOtp } = require("../controller/sendOtp"); // Updated import
+const verifyOtp = require("../controller/verifyOtp");
 
 const router = express.Router();
 
@@ -34,6 +36,10 @@ router.post("/search-user", searchUser);
 
 // Search Friend & User
 router.post("/search-friend-user", searchFriendUser);
+
+// OTP routes
+router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
 
 // Logout user
 router.get("/logout", logout);
